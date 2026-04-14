bf69f91011afbb549eacf237d1acfc9d995a634d
O-S-O-01 <paulohajekwe@yahoo.com>
2026-04-14 20:41:24 +0100

    update

---
+++ libraries/game/game.py
@@ -0,0 +1,30 @@
+def main():
+	import random
+	while True:
+		try:
+			level = int(input('Level: '))
+			if level > 0:
+				break
+# we use except valueerror here because 'input' accept only strings, so if a string is typed we get a value error becaue of the int tag on line four  
+		except ValueError:
+			continue
+
+	number = random.randint (1, level)
+
+	while True:
+		guess = input('what is your guess: ')
+		if not guess.isdigit():
+			continue
+		guess= int(guess)
+
+		if guess < number:				
+			print ('Too small!')
+		elif guess > number:
+			print ('Too large!')
+		else:
+			print('Just right!')
+		break
+
+	
+if __name__=='__main__':
+	main()
