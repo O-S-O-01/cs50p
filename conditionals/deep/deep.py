79bc3b04ddcf6c344a48810e09ccba54729d5dd6
O-S-O-01 <paulohajekwe@yahoo.com>
2026-04-26 18:44:46 +0100

    update

---
+++ conditionals/deep/deep.py
@@ -0,0 +1,7 @@
+life_question =  input('what is the answer to the great question of life, the universe and everything?')
+answer= life_question.strip().lower()
+
+if answer == '42' or answer == 'forty-two' or answer == 'forty two':
+	print ('yes')
+else:
+	print('no')
