24dbafaa4c8b2cf024eab1a47315c2974395644d
O-S-O-01 <paulohajekwe@yahoo.com>
2026-04-26 18:04:06 +0100

    update

--- loops/twttr/twttr.py
+++ loops/twttr/twttr.py
@@ -1,6 +1,15 @@
-text = input('what is your message? ' )
-result=''
-for t in text:
-	if t.lower() not in ['a', 'e', 'i', 'o', 'u' ]:
-		result += t
-print (result)
+def main():
+    text = input("What is your message? ")
+    print(shorten(text))
+
+
+def shorten(word):
+    result = ''
+    for t in word:
+        if t.lower() not in ['a', 'e', 'i', 'o', 'u']:
+            result += t
+    return result
+
+
+if __name__ == "__main__":
+    main()
